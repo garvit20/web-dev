1ff17572bedb70d6c0bd68a87cea022705415ed9
garvit20 <68471396+garvit20@users.noreply.github.com>
2020-10-30 02:04:03 +0530

    Update script.js

--- module-4-solution/js/script.js
+++ module-4-solution/js/script.js
@@ -1,8 +1,7 @@
 (function(){
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var i = 0; i < names.length; i++) {
-		var firstLetter = names[i].charAt(0);
-		firstLetter = firstLetter.toLowerCase();
+		var firstLetter = names[i].charAt(0).toLowerCase();
 		if(firstLetter == "j"){
 			byeSpeaker.speak(names[i]);
 		}
